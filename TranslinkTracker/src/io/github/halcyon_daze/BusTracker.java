041c37a1abe26da78d8e9ec998eca1ebe219ee87
Christopher Chin <christoph.chin@gmail.com>
2017-08-27 02:41:19 -0700

    Can retrieve data from http request

--- TranslinkTracker/src/io/github/halcyon_daze/BusTracker.java
+++ TranslinkTracker/src/io/github/halcyon_daze/BusTracker.java
@@ -1,23 +1,18 @@
 package io.github.halcyon_daze;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class BusTracker {
     
     public static void main(String[] args) {
-        getTimes("https://nb.translink.ca/text/stop/58088/route/028#");
+        getTimes("https://nb.translink.ca/nextbus.ashx?cp=gssr%2FSeI8rdrYzRqLxH%2FfgZYTGg==;028");
         //getTimes("https://ca.yahoo.com/");
 
     }
@@ -27,24 +22,44 @@ public class BusTracker {
         System.out.println("Connecting to " + url);
         
         try {
-            WebClient webClient = new WebClient(BrowserVersion.EDGE);
-            webClient.getOptions().setThrowExceptionOnScriptError(false);
-            webClient.getOptions().setJavaScriptEnabled(true);
-
-            webClient.waitForBackgroundJavaScript(100000);
             
-            HtmlPage page = webClient.getPage(url);
-            webClient.close();
+            URL pageURL = new URL(url);
             
-            Document docPage = Jsoup.parse(page.asXml());
+            HttpURLConnection urlConnection = (HttpURLConnection) pageURL.openConnection();
+            urlConnection.setRequestProperty("Cookie", "_utsma=61047657.704264432.1503808521.1503808521.1503812936.2;_utmz=61047657.1503808521.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none);ASP.NET_Sessionid=xst54zciexwzgrgr4eg5pzxa;ARRAffinity=d930dc5fc3a61ca4548df4822e7cff4111b8d71cc60cb328700427e4bfd458f9;nb.cookie=enabled;_utmb=61047657.1.10.1503812936;_utmc=61047657;_utmt=1;_ga=GA1.2.704264432.1503808521;_gid=GA1.2.1990847912.1503812943");
+            urlConnection.setRequestMethod("GET");
             
-            Elements schedule = docPage.select("div.schedule");
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            String result = convertStreamToString(in);
             
-            System.out.println("Printing out schedule: \n" + docPage.toString());
-            
+            System.out.print(result.toString());
+                    
         } catch (IOException e) {
             System.out.println("Could not extract info from " + url);
             return; 
         }
+    }
+    
+    private static String convertStreamToString (InputStream in) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder sb = new StringBuilder();
+        
+        String input;
+        
+        try {
+            while ((input = reader.readLine()) != null) {
+                sb.append(input).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+            in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return sb.toString();
     }
 }
